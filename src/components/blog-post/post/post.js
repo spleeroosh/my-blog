599c3b652622fa8fb935b1cfbd40218d23584cdb
Pavel Rogovin <rogovin.pavel@gmail.com>
2019-07-16 19:35:35 +0300

    Some refactoring in the blog-post component.

---
+++ src/components/blog-post/post/post.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './post.css';
+
+import removeButton from './../../../images/remove-button.svg';
+
+const Post = ({ newPost, onDeletePost }) => {
+  
+  const { id, title, post } = newPost;
+
+  return (
+    <div className="card bg-white mg-auto post" key={id}>
+      <div className="card-header post__header">
+        <h4 className="post__heading">{title}</h4>
+        <div className="remove-btn"
+              onClick={() => onDeletePost(id)}>
+          <img src={removeButton} alt="remove button" className="remove-btn__image"/>
+        </div>
+      </div>
+      <div className="card-body post__body">
+        <p className="post__paragraph text-dark">{post}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
