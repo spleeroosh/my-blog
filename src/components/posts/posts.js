fc86826ba77abd8886ac82bc30a9d18bfca8409b
Pavel Rogovin <rogovin.pavel@gmail.com>
2020-01-16 22:14:41 +0300

    posts.js post.scss поправил стили поста

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import { connect } from 'react-redux';
-import { removePost, postsLoaded } from './../../actions';
+import { connect } from "react-redux";
+import { removePost, postsLoaded } from "./../../actions";
 
-import AddPost from '../add-post';
-import Post from './post';
+import AddPost from "../add-post";
+import Post from "./post";
 
 class Posts extends Component {
-
   /**
    * @param {Number} id Получаем id статьи,
    * удаляем статью из массива, и диспатчим новый массив со статьями
    */
-  onDeletePost = (id) => {
+  onDeletePost = id => {
     const { dispatch } = this.props;
     const { posts } = this.props;
     const newPosts = posts.filter(post => post.id !== id);
-    const removedPost = posts.reduce((removed_post, post) => removed_post.id === id ? removed_post : post, {});
+    const removedPost = posts.reduce(
+      (removed_post, post) => (removed_post.id === id ? removed_post : post),
+      {}
+    );
 
     dispatch(removePost(newPosts, removedPost));
-  }
+  };
 
   /**
    * Загружаем данные по статьям, когда компонент отрисовался
    */
   componentDidMount() {
     const { dispatch } = this.props;
-    
+
     dispatch(postsLoaded());
   }
 
@@ -35,27 +37,36 @@ class Posts extends Component {
     const { posts, user } = this.props;
     const { onDeletePost } = this;
     const loading = <div className="sign-out loader">loading...</div>;
-    const postsComponent = <section className="posts container">
-      {posts.map(post => <Post post={post} user={user} onDeletePost={onDeletePost} key={post['id']}/>)}
-    </section>;
-    
+    const postsComponent = (
+      <section className="posts container">
+        {posts.map(post => (
+          <Post
+            post={post}
+            user={user}
+            onDeletePost={onDeletePost}
+            key={post['id']}
+          />
+        ))}
+      </section>
+    );
+
     return (
       <React.Fragment>
-        { posts.length ? postsComponent : loading }
+        {posts.length ? postsComponent : loading}
         <AddPost />
       </React.Fragment>
     );
   }
 }
 
-const mapStateToProps = ( state ) => {
+const mapStateToProps = state => {
   return {
     posts: state.posts,
     user: state.user
   };
 };
 
-const mapDispatchToProps = ( dispatch ) => {
+const mapDispatchToProps = dispatch => {
   return {
     dispatch
   };
