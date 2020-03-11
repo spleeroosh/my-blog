7afcb33ba83c38b5c295488761a9c4f08aa54051
Rogovin-Pavel <rogovin.pavel@gmail.com>
2020-03-11 09:15:40 +0300

    description logic + css

--- src/components/about/item/description/description.js
+++ src/components/about/item/description/description.js
@@ -5,14 +5,25 @@ const Description = ({ description, description_list }) => {
   let Description_list = null;
   if (description_list) {
     Description_list = <ul className='about__description-list'>
-      { _.map(description_list, (desc, key) => <li className={ key }>{ desc }</li>) }
+      { _.map(description_list, (desc, key) => {
+        if (_.isObject(desc)) {
+          const { description_title, description } = desc;
+
+          return <li className={ key }>
+            <h5 className='about__description-title'>{ description_title }</h5>
+            <p className='about__description'>{ description }</p>
+          </li>;
+        } else {
+          return <li className={ key }>{ desc }</li>;
+        }
+      }) }
     </ul>;
   }
   
-  return <React.Fragment>
+  return (<React.Fragment>
     <div className='about__description'>{ description }</div>
     {Description_list}
-  </React.Fragment>;
+  </React.Fragment>);
 };
 
 export default Description;
